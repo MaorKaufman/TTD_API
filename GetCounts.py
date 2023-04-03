e9e9c1fae39b2a0f61776d96f4d3bc6d3160f9ef
MaorKaufman <123461794+MaorKaufman@users.noreply.github.com>
2023-04-03 15:33:23 +0300

    Update GetCounts.py

--- GetCounts.py
+++ GetCounts.py
@@ -11,8 +11,8 @@ def intro():
 
 page_names_to_funcs = {
     "—": intro,
-    "Get Counts": get_counts
-  #  "Plotting Demo": plotting_demo,
+   # "Get Counts": get_counts
+  # "Plotting Demo": plotting_demo,
    # "Mapping Demo": mapping_demo,
    # "DataFrame Demo": data_frame_demo
 }
