67f09c39884af940aa71a66081d028799b95b814
MaorKaufman <123461794+MaorKaufman@users.noreply.github.com>
2023-04-03 12:29:28 +0300

    Update GetCounts.py

--- GetCounts.py
+++ GetCounts.py
@@ -15,9 +15,10 @@ page_names_to_funcs = {
 }
 
 st.sidebar.title(" Welcome to Start.io's TTD's UI tool")
+st.sidebar.subheader("""This tools was created to enable Start.io's team to get details/ update custom/prepack segments on TTD's side.""")
+
 demo_name = st.sidebar.selectbox("Choose a UI", page_names_to_funcs.keys())
 page_names_to_funcs[demo_name]()    
-st.sidebar.subheader("""This tools was created to enable Start.io's team to get details/ update custom/prepack segments on TTD's side.""")
 st.sidebar.success("Select a UI tool.")
 
 
