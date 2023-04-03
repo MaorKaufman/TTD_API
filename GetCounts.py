731fab871cc93377bc7193029e0d79be7efbae58
MaorKaufman <123461794+MaorKaufman@users.noreply.github.com>
2023-04-03 15:30:14 +0300

    Update GetCounts.py

--- GetCounts.py
+++ GetCounts.py
@@ -26,7 +26,7 @@ st.sidebar.success("Select a UI tool.")
 
 
 #
-def get_counts()
+def get_counts():
     st.subheader("Get segment count API")
     segID= st.text_input("Please insert SegmentID: ")
 
