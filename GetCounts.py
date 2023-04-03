b9e16cb4099c0507ff018843c73ccb8e4803d5af
MaorKaufman <123461794+MaorKaufman@users.noreply.github.com>
2023-04-03 14:22:06 +0300

    Update GetCounts.py

--- GetCounts.py
+++ GetCounts.py
@@ -77,8 +77,7 @@ response = requests.post(url=API_URL ,headers=Headers ,data=json_data)
 # Check if the request was successful
 if response.status_code >= 200 and response.status_code<300:
     data = response.json() 
-    if not segID:
-         st.write(data)
+    st.write(data)
 else:
     st.write("could not retrieve data")
     
