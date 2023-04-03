390f34d0908a3dca1d93cf7a60a389fc5cd95058
MaorKaufman <123461794+MaorKaufman@users.noreply.github.com>
2023-04-03 15:15:53 +0300

    Update GetCounts.py

--- GetCounts.py
+++ GetCounts.py
@@ -14,7 +14,6 @@ page_names_to_funcs = {
    # "Mapping Demo": mapping_demo,
    # "DataFrame Demo": data_frame_demo
 }
-st.sidebar.container():
 image = Image.open(‘C:/Users/MaorKaufman/Downloads/StartLogo.jpg’)
 st.sidebar.image(image, width= use_column_width=True)
 st.sidebar.title(" Welcome to Start.io's TTD's UI tool")
