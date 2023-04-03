16c0627666910dac33c92687fbffd67109ba5e66
MaorKaufman <123461794+MaorKaufman@users.noreply.github.com>
2023-04-03 09:16:44 +0300

    Update GetCounts.py

--- GetCounts.py
+++ GetCounts.py
@@ -54,7 +54,7 @@ response = requests.post(url=API_URL ,headers=Headers ,data=json_data)
 # Check if the request was successful
 if response.status_code >= 200 and response.status_code<300:
     data = response.json()
-    df = pd.DataFrame(data)
+    #df = pd.DataFrame(data)
     st.write(data)
 else:
     st.write("could not retrieve data")
