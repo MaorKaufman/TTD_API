1c9d9a11efd4dbe7d7d2d8557f6c7b69a476b503
MaorKaufman <123461794+MaorKaufman@users.noreply.github.com>
2023-04-03 09:10:03 +0300

    Update GetCounts.py

--- GetCounts.py
+++ GetCounts.py
@@ -12,7 +12,7 @@ data= {"Login": "ttd_api_startapp@startapp.com",
 "Password": "sodaCSM2019!", 
 "TokenExpirationInMinutes": 1440.0}
 #
-st.write(type(json.dumps(data)))
+# st.write(type(json.dumps(data)))
 json_data = json.dumps(data)
 #
 Headers = {"Content-Type": "application/json"}
@@ -23,30 +23,31 @@ response = requests.post(url=API_URL ,headers=Headers ,data=json_data)
 if response.status_code >= 200 and response.status_code<300:
     data= response.json()  
     auth = data['Token']
-    st.write("the token is"+ auth)
+  #  st.write("the token is"+ auth)
 else:
-        st.write(response.status_code)
-        st.write(response.json()['Token']) 
+     #   st.write(response.status_code)
+      #  st.write(response.json()['Token']) 
         st.write("Error: Could not retrieve Token from TTD.")
     
 #
 API_URL="https://api.thetradedesk.com/v3/thirdpartydata/query"
-#
-Headers = {"TTD-Auth": auth,
-           "Content-Type": "application/json"}
        
 #Start process
 segID=input("Please insert SegmentID:")
-
+#
+Headers = {"TTD-Auth": auth,
+           "Content-Type": "application/json"}
 # data
 data = {"ProviderId": "startapp",
   "ProviderElementId":segID ,
   "IncludeActiveIDsCountExpandedFlag":" true",
   "PageStartIndex":"0",
   "PageSize": "100"}
- 
- # Check if the request was successful
-if response.status_code == 200:
+#
+response = requests.post(url=API_URL ,headers=Headers ,data=json_data)
+
+# Check if the request was successful
+if response.status_code >= 200 and response.status_code<300:
     data = response.json()
     df = pd.DataFrame(data)
   
