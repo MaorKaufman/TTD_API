129d041b01f14ee931919c50fae442b9c1acd3ca
MaorKaufman <123461794+MaorKaufman@users.noreply.github.com>
2023-04-03 10:00:54 +0300

    Update GetCounts.py

--- GetCounts.py
+++ GetCounts.py
@@ -55,7 +55,7 @@ response = requests.post(url=API_URL ,headers=Headers ,data=json_data)
 # Check if the request was successful
 if response.status_code >= 200 and response.status_code<300:
     data = response.json()
-    df = pd.json_normalize(data)
+    df = pd.json_normalize(data, sep='_')
     st.table(df)  
  #   st.write(data)
 else:
